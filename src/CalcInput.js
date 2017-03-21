28c23ea4a4a5857b6dca76ce03063995cf500e31
nik frank <nikf@sweetinn.com>
2017-03-21 21:43:33 +0200

    simulate clicks on inputs

--- src/CalcInput.js
+++ src/CalcInput.js
@@ -6,7 +6,8 @@ class CalcInput extends Component {
       <div className="calc-input">
         {
           [1,2,3,4,5,6,7,8,9,0].map( i => (
-            <button key={i}
+            <button key={'number'+i}
+                    className="calc-input-number"
                     onClick={()=>this.props.onNumber(i)}>
               {i}
             </button>
@@ -14,14 +15,21 @@ class CalcInput extends Component {
         }
         {
           ['+', '-', '*', '/', '%'].map( f => (
-            <button key={f}
+            <button key={'func'+f}
+                    className="calc-input-func"
                     onClick={()=>this.props.onFunc(f)}>
               {f}
             </button>
           ) )
         }
-        <button onClick={this.props.onEq}>=</button>
-        <button onClick={this.props.onClear}>C</button>
+        <button onClick={this.props.onEq}
+                className="calc-input-eq">
+          =
+        </button>
+        <button onClick={this.props.onClear}
+                className="calc-input-clear">
+          C
+        </button>
       </div>
     );
   }
--- src/CalcInput.test.js
+++ src/CalcInput.test.js
@@ -10,3 +10,48 @@ it('should render the input buttons', () => {
   );
   expect(wrapper).toMatchSnapshot();
 });
+
+it('calls the right functions', ()=>{
+  // init spy functions
+  const onNumber = jest.fn();
+  const onFunc = jest.fn();
+  const onClear = jest.fn();
+  const onEq = jest.fn();
+
+  // shallow render
+  const wrapper = shallow(
+    <CalcInput onNumber={onNumber}
+               onFunc={onFunc}
+               onClear={onClear}
+               onEq={onEq}/>
+  );
+
+  // simulate clicks and make expectations
+  
+  wrapper.find('.calc-input-number')
+         .first()
+         .simulate('click');
+
+  expect(onNumber).toBeCalledWith(1);
+
+  
+  wrapper.find('.calc-input-func')
+         .at(4)
+         .simulate('click');
+
+  expect(onFunc).toBeCalledWith('%');
+
+
+  wrapper.find('.calc-input-eq')
+         .simulate('click');
+
+  expect(onEq.mock.calls.length).toEqual(1);
+
+
+  const clearBtn = wrapper.find('.calc-input-clear');
+
+  clearBtn.simulate('click');
+  clearBtn.simulate('click');
+
+  expect(onClear.mock.calls.length).toEqual(2);
+});
